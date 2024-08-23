0e0bc3e23ab5d24fc225d39a1973e9d9fd3a1eb1
Jonas Lima <jonaslima23122003.com>
2024-08-22 23:21:11 -0300

    primeiro commit

---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+/* existing imports */
+import Root from "./routes/root";
+/* previous imports */
+import ErrorPage from "./error-page";
+/* existing imports */
+import Contact from "./routes/contact";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
